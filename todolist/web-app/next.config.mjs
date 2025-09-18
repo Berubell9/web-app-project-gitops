bd4e56c457de354967a61605ff05178fe88d6b2d
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2025-09-18 12:20:33 +0700

    web

--- todolist/web-app/next.config.mjs
+++ todolist/web-app/next.config.mjs
@@ -1,10 +1,4 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", 
-        destination: "http://localhost:5000/api/:path*", 
-      },
-    ];
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
